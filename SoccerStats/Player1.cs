a348c2db7b7d6358eff0f61cc7a137bcb685ca95
harry <harryddye@hotmail.com>
2019-01-24 15:36:22 +0000

    Add project files.

---
+++ SoccerStats/Player1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SoccerStats
+{
+
+    public class RootObject
+    {
+        public Player[] Player { get; set; }
+    }
+
+    public class Player
+    {
+        
+        [JsonProperty(PropertyName = "first_name")]
+        public string FirstName { get; set; }
+       
+        public int id { get; set; }
+       
+        public string points_per_game { get; set; }
+        
+        public string second_name { get; set; }
+        
+        public string team_name { get; set; }
+        
+    }
+
+}
